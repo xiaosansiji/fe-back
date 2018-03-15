c320218deeb0677e466e88133b99a643071467ce
xiaosansiji <799552341@qq.com>
2018-03-15 10:54:51 +0800

    allow cross origin xhr

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const app = new Koa()
 
 app.use(async (ctx) => {
     const path = ctx.request.url
-    console.log(ctx.request)
     if (path === '/read.gif') {
         ctx.body = 'ok'
     } else if (path === '/post/jserr') {
@@ -11,6 +10,8 @@ app.use(async (ctx) => {
     } else {
         ctx.body = 'hello koa2'
     }
+    // 允许 post 请求可以跨域提交
+    ctx.response.set("Access-Control-Allow-Origin", ctx.request.header.referer.replace(/\/$/gi, ""))
 })
 
 app.listen(3000)
