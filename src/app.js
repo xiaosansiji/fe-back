1badfca87a074aeda604eb08d55a7471fa14757b
xiaosansiji <799552341@qq.com>
2018-03-15 14:23:31 +0800

    add koa-body middleware

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
 const Koa = require('koa')
+const koaBody = require('koa-body')
 const app = new Koa()
 
+app.use(koaBody())
 app.use(async (ctx) => {
-    const path = ctx.request.url
+    const path = ctx.request.path
     if (path === '/read.gif') {
         ctx.body = 'ok'
-    } else if (path === '/post/jserr') {
-        ctx.body = 'ok'
+        console.log("GET: ", ctx.request.query)
+    } else if (path === '/post/webper') {
+        console.log("POST: ", ctx.request.body)
+        ctx.response.body = 'ok'
+        
     } else {
         ctx.body = 'hello koa2'
     }
     // 允许 post 请求可以跨域提交
-    ctx.response.set("Access-Control-Allow-Origin", ctx.request.header.referer.replace(/\/$/gi, ""))
+    ctx.response.set("Access-Control-Allow-Origin", ctx.request.header.origin)
 })
 
 app.listen(3000)
